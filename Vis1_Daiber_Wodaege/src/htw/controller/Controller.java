532ab95d01841204a2d946f0271069500f7d7e34
mdaiber <s0558471@htw-berlin.de>
2018-04-22 17:10:09 +0200

    adds more menupoints and tests.

--- Vis1_Daiber_Wodaege/src/htw/controller/Controller.java
+++ Vis1_Daiber_Wodaege/src/htw/controller/Controller.java
@@ -10,14 +10,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Controller {
@@ -26,9 +25,11 @@ public class Controller {
 	int menuFlag = 0;
 
 	private enum MenuEntries {
-		CIRCILE("Circle"),
-		SQUARE("Square"),
-		PROGRAM("Program");
+		CIRCLE("Kreis"),
+		RAD_CIRCLE("Kreis mit Radius"),
+		SQUARE("Viereck"),
+		GUESS_CIRCLE("Verhältnis Kreisflächen");
+		//PROGRAM("Program");
 
 		private final String name;
 
@@ -48,7 +49,10 @@ public class Controller {
 	private Slider slider;
 
 	@FXML
-	private Button button;
+	private Button btnCalc;
+
+	@FXML
+	private Button btnNext;
 
 	@FXML
 	private Canvas canvas;
@@ -59,16 +63,28 @@ public class Controller {
 	@FXML
 	private Label infoLabel;
 
+	@FXML
+	private Label lblEstimate;
+
+	@FXML
+	private TextField tfEstimate;
+
 	private GraphicsContext gc;
 
-	private int canvasHeight = 600;
-	private int canvasWidth = 800;
+	private int canvasHeight = 300;
+	private int canvasWidth = 400;
 	private int canvasArea = canvasHeight * canvasWidth;
 
 	private int randFixedArea;
+	private int shapeSize = 0;
+	private int circleRatio = 0;
+
+	private List<Double> overallRatio;
 
 	@FXML
-	public void initialize() {
+	public void initialize()
+	{
+		overallRatio = new LinkedList<>();
 
 		infoLabel.setText("Waehlen Sie einen Test aus.");
 		menuEntries.setItems(FXCollections.observableArrayList(MenuEntries.values()));
@@ -79,100 +95,121 @@ public class Controller {
 		gc = canvas.getGraphicsContext2D();
 
 		slider.setMin(1);
-		slider.setMax(12);
+		slider.setMax(10);
 
-		button.setText("Ergebnis anzeigen");
+		btnCalc.setText("Ergebnis anzeigen");
+		btnNext.setText("Nächter Versuch");
 		resultLabel.setText("Ergebnis");
 
-		randFixedArea = ThreadLocalRandom.current().nextInt(canvasArea / 150, canvasArea / 60);
+		btnNext.setDisable(true);
+		lblEstimate.setVisible(false);
+		tfEstimate.setVisible(false);
+
+
+		randFixedArea = ThreadLocalRandom.current().nextInt(canvasArea / 80, canvasArea / 20);
+		shapeSize = ThreadLocalRandom.current().nextInt(2, 11);
 
 		slider.valueProperty().addListener(new ChangeListener() {
 
 			@Override
 			public void changed(ObservableValue observable, Object oldValue, Object newValue) {
 
-				programChange();
-				/*
-				if (menuFlag == true) {
-					drawScaleableCircles(slider.getValue(), true);
-				}
-				if (squareFlag == true) {
-					drawScaleableSquares(slider.getValue());
-				}
-
-				System.out.println(slider.getValue());
-				*/
+				shapeChange();
 			}
 		});
 	}
 
 	@FXML
-	private void buttonClick() {
-		double result = calculateResult(slider.getValue());
+	private void calcRatio()
+	{
+		double meanRatio = 0;
+		double currentRatio = 0;
+		for(int i = 0; i < overallRatio.size(); ++i)
+		{
+			meanRatio += overallRatio.get(i);
+		}
 
-		String resultAsString = df2.format(result);
+		meanRatio /= overallRatio.size();
 
-		resultLabel.setText(resultAsString);
+		switch (menuEntries.getValue()) {
+			case CIRCLE:
+			case SQUARE:
+			case RAD_CIRCLE:
+				currentRatio = calculateResult(randFixedArea * shapeSize, randFixedArea * slider.getValue());
+				break;
+			case GUESS_CIRCLE:
+				if(tfEstimate.getText().isEmpty()) {
+					infoLabel.setText("Bitte etwas als Schätzung eingeben!");
+					break;
+				}
+				else {
+					// TODO ungültige eingaben escapen
+					double guessedRatio = Double.valueOf(tfEstimate.getText());
+
+					currentRatio = calculateResult(circleRatio, guessedRatio);
+					break;
+				}
+
+		}
+
+		// TODO value
+		double currentResult = calculateResult(currentRatio, shapeSize);
+		double meanResult = calculateResult(meanRatio, shapeSize);
+
+		String currentResultAsString = df2.format(currentRatio);
+		String meanResultAsString = df2.format(meanRatio);
+
+		resultLabel.setText("Die aktuelle Ratio ist: "+ currentResultAsString + " die bisherigen Ratios im Schnitt sind: "+meanRatio);
 
 	}
 
-	private void programChange() {
+	private void shapeChange() {
 
 		switch (menuEntries.getValue()) {
-			case CIRCILE:
-				infoLabel.setText("Bitte ziehen Sie den roten Kreis auf bis dieser im Verhaeltnis \n die dreifache Groesse des schwarzen Kreises hat.");
-				drawScaleableCircles(slider.getValue(), true);
+			case CIRCLE:
+				infoLabel.setText("Bitte ziehen Sie den roten Kreis auf bis dieser im Verhaeltnis \n die "+shapeSize+"-fache Groesse des schwarzen Kreises hat.");
+				drawScaleableCircles(slider.getValue(), false);
 				break;
 			case SQUARE:
-				infoLabel.setText("Bitte ziehen Sie das rote Quadrat auf bis dieses im Verhaeltnis \n die dreifache Groesse des schwarzen Quadrates hat.");
+				infoLabel.setText("Bitte ziehen Sie das rote Quadrat auf bis dieses im Verhaeltnis \n die "+shapeSize+"-fache Groesse des schwarzen Quadrates hat.");
 				drawScaleableSquares(slider.getValue());
 				break;
-			case PROGRAM:
+			case RAD_CIRCLE:
+				infoLabel.setText("Bitte ziehen Sie den roten Kreis auf bis dieser im Verhaeltnis \n die "+shapeSize+"-fache Groesse des schwarzen Kreises hat.");
+				drawScaleableCircles(slider.getValue(), true);
 				break;
-
+			case GUESS_CIRCLE:
+				infoLabel.setText("Schätzen Sie um wieviel groesser der schwarze Kreis im Vergleich \n zum roten Kreis ist.");
+				slider.setDisable(true);
+				lblEstimate.setVisible(true);
+				tfEstimate.setVisible(true);
+				drawCircles();
+				break;
+			/*
+			case PROGRAM:
+				btnNext.setDisable(false);
+				drawProgram();
+				break;
+			*/
 			default:
 				break;
 		}
 
 	}
 
-
-
 	@FXML
 	public void chooseMenuEntry() {
 
-		resetGui();
-		programChange();
+		lblEstimate.setVisible(false);
+		tfEstimate.setVisible(false);
+		slider.setDisable(false);
 
-		/*
-		switch (menuEntries.getValue()) {
-		case CIRCILE:
-			circleFlag = true;
-			squareFlag = false;
-			resetGui();
-			infoLabel.setText("Bitte ziehen Sie den roten Kreis auf bis dieser im Verhaeltnis \n die dreifache Groesse des schwarzen Kreises hat.");
-			drawScaleableCircles(slider.getValue(), true);
-			break;
-		case SQUARE:
-			squareFlag = true;
-			circleFlag = false;
-			resetGui();
-			infoLabel.setText("Bitte ziehen Sie das rote Quadrat auf bis dieses im Verhaeltnis \n die dreifache Groesse des schwarzen Quadrates hat.");
-			drawScaleableSquares(slider.getValue());
-			break;
-		case PROGRAM:
-			break;
-
-		default:
-			break;
-		}
-		*/
-	}
-
-	public void resetGui() {
 		resultLabel.setText("Ergebnis");
+		overallRatio = new LinkedList<>();
 		slider.setValue(1);
 		gc.clearRect(0, 0, canvasWidth, canvasHeight);
+		btnNext.setDisable(false);
+		nextShape();
 	}
 
 	/*
@@ -191,7 +228,6 @@ public class Controller {
 
 		fixSquare.draw(gc, 0, canvasHeight / 4);
 		scalableSquare.draw(gc, sideFixedSquare*2, canvasHeight / 4);
-
 	}
 
 	private void drawScaleableCircles(double sliderValue, boolean drawRadius) {
@@ -207,9 +243,8 @@ public class Controller {
 		fixCircle.drawCircle(gc, 0, canvasHeight / 4);
 		scaleableCircle.drawCircle(gc, radiusFixedCircle*2, canvasHeight / 4);
 
-		if (drawRadius) {
-
-
+		if (drawRadius)
+		{
 			// radius small circle
 			double x1 = radiusFixedCircle / 2;
 			double y1 = canvasHeight / 4 + radiusFixedCircle / 2;
@@ -232,15 +267,15 @@ public class Controller {
 	}
 
 	// generate two cirles with random numbers as radius and return the ratio
-	private int drawCircles() {
+	private void drawCircles() {
 
 		gc.clearRect(0, 0, canvasWidth, canvasHeight);
 
 		int randAreaSmallCircle = ThreadLocalRandom.current().nextInt(canvasArea / 150, canvasArea / 60);
-		int ratio = ThreadLocalRandom.current().nextInt(2, 12);
+		circleRatio = ThreadLocalRandom.current().nextInt(2, 12);
 
 		// big cirle should be a 'even' multiple of small circle
-		int randAreaBigCircle = randAreaSmallCircle * ratio;
+		int randAreaBigCircle = randAreaSmallCircle * circleRatio;
 
 		double radiusSmallCircle = Math.sqrt(randAreaSmallCircle);
 		double radiusBigCircle = Math.sqrt(randAreaBigCircle);
@@ -253,15 +288,84 @@ public class Controller {
 
 		smallCircle.drawCircle(gc, 0, canvasHeight / 4);
 		bigCircle.drawCircle(gc, radiusSmallCircle, canvasHeight / 4);
-
-		return ratio;
 	}
 
-	// berechne x f�r (wahrgenommenes verh�ltnis) = (tats�chliches verh�ltnis)^x
-	private double calculateResult(double astimatedRatio) {
-		int realRation = 3;
-		double result;
-		return result = Math.log(astimatedRatio) / Math.log(realRation);
+	/*
+	@FXML
+	public void drawProgram()
+	{
+		switch (programCounter){
+			case 0:
+				infoLabel.setText("Dieses Program führt Sie durch einige Aufgaben, bei denen  \n " +
+									"der Groessenunterschied von Zwei Formen bestimmt werden soll. \n\n " +
+									"Click 'Nächste' für nächsten Schritt"  );
+				break;
+			case 1:
+				infoLabel.setText("Bitte ziehen Sie das rote Quadrat auf bis dieses im Verhaeltnis \n die dreifache Groesse des schwarzen Quadrates hat.");
+				drawScaleableSquares(slider.getValue());
+				programRatios.add(calculateResult(3, ratio));
+				System.out.println("1 ratio: "+ratio);
+				// TODO error label for leeres feld
+				break;
+			case 3:
+				infoLabel.setText("Schätzen Sie um wieviel der schwarze Kreis größer ist als der rote. \n Click 'Nächste' für nächsten Schritt");
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+
+			default:
+				infoLabel.setText("program nix gut");
+				break;
+		}
+
+		programCounter++;
+	}
+	*/
+
+	@FXML
+	private void nextShape()
+	{
+		switch (menuEntries.getValue()) {
+			case CIRCLE:
+			case SQUARE:
+			case RAD_CIRCLE:
+				overallRatio.add(calculateResult(randFixedArea * shapeSize, randFixedArea * slider.getValue()));
+				newShapes();
+				break;
+			case GUESS_CIRCLE:
+				if(tfEstimate.getText().isEmpty() && tfEstimate.isVisible()) {
+					infoLabel.setText("Bitte etwas als Schätzung eingeben!");
+					break;
+				}
+				else {
+					// TODO ungültige eingaben escapen
+
+					if(tfEstimate.isVisible()) // for initialisation
+					{
+						double guessedRatio = Double.valueOf(tfEstimate.getText());
+						overallRatio.add(calculateResult(circleRatio, guessedRatio));
+					}
+
+					tfEstimate.clear();
+					newShapes();
+					break;
+				}
+		}
+	}
+
+	private void newShapes()
+	{
+		// generate a random number for the value which the user should guess
+		shapeSize = ThreadLocalRandom.current().nextInt(2, 11);
+		shapeChange();
+	}
+
+	// berechne x fuer (wahrgenommenes verh�ltnis) = (tats�chliches verh�ltnis)^x
+	private double calculateResult(double realRation , double estimatedRatio) {
+
+		return Math.log(estimatedRatio) / Math.log(realRation);
 
 	}
 }
